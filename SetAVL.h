6b6d017fa55ac658b449831e032b339bde1894e1
IgorAmashukeli <amashukeliim1150@gmail.com>
2025-08-18 01:58:55 +0300

    rewrite_rule_o_in_its

--- SetAVL.h
+++ SetAVL.h
@@ -216,13 +216,6 @@ public:
 
     class Iterator {
     public:
-        Iterator() noexcept = default;
-        Iterator(const Iterator& other) noexcept = default;
-        Iterator& operator=(const Iterator& other) noexcept = default;
-        Iterator(Iterator&& other) noexcept = default;
-        Iterator& operator=(Iterator&& other) noexcept = default;
-        ~Iterator() noexcept = default;
-
         explicit Iterator(SetBaseNode<K>* node) noexcept : node_(node) {
         }
         Reference operator*() const {
@@ -276,13 +269,6 @@ public:
 
     class ConstIterator {
     public:
-        ConstIterator() noexcept = default;
-        ConstIterator(const ConstIterator& other) noexcept = default;
-        ConstIterator& operator=(const ConstIterator& other) noexcept = default;
-        ConstIterator(ConstIterator&& other) noexcept = default;
-        ConstIterator& operator=(ConstIterator&& other) noexcept = default;
-        ~ConstIterator() noexcept = default;
-
         explicit ConstIterator(const SetBaseNode<K>* node) noexcept : node_(node) {
         }
         ConstIterator(Iterator it) noexcept : node_(it.node_) {
@@ -338,13 +324,6 @@ public:
 
     class ReverseIterator {
     public:
-        ReverseIterator() noexcept = default;
-        ReverseIterator(const ReverseIterator& other) noexcept = default;
-        ReverseIterator& operator=(const ReverseIterator& other) noexcept = default;
-        ReverseIterator(ReverseIterator&& other) noexcept = default;
-        ReverseIterator& operator=(ReverseIterator&& other) noexcept = default;
-        ~ReverseIterator() noexcept = default;
-
         explicit ReverseIterator(SetBaseNode<K>* node) : node_(node) {
         }
         Reference operator*() const {
@@ -398,13 +377,6 @@ public:
 
     class ConstReverseIterator {
     public:
-        ConstReverseIterator() noexcept = default;
-        ConstReverseIterator(const ConstReverseIterator& other) noexcept = default;
-        ConstReverseIterator& operator=(const ConstReverseIterator& other) noexcept = default;
-        ConstReverseIterator(ConstReverseIterator&& other) noexcept = default;
-        ConstReverseIterator& operator=(ConstReverseIterator&& other) noexcept = default;
-        ~ConstReverseIterator() noexcept = default;
-
         explicit ConstReverseIterator(const SetBaseNode<K>* node) noexcept : node_(node) {
         }
         ConstReverseIterator(ReverseIterator it) noexcept : node_(it.node_) {
